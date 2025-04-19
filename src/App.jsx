f8bc760649983ed9ba4abc3ed2773d80f0ecca03
Vladislav Susikov <susikovvlad@gmail.com>
2025-04-19 19:07:09 +0300

    alisha

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
     >
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          ЛЕЙЛАЛЕЙЛА
           <Navbar />
           <Hero />
         </div>
