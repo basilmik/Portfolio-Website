1c03a2a26522730fa641a3e405c7c70b4e8039fc
Vladislav Susikov <susikovvlad@gmail.com>
2025-04-19 19:09:53 +0300

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,6 +49,7 @@ const Navbar = () => {
 
   return (
     <>
+     ЛЕЙЛАЛЕЙЛА
       <nav
         className={`${styles.paddingX} w-full flex items-center py-3 fixed top-0 z-20 bg-primary`}
       >
